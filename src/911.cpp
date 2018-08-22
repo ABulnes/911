aea81b1b2eaf874fc4106c6b1bdf5ba6b052681e
root <root@debian>
2018-08-22 03:58:13 -0600

    Modificacion a 911.cpp y creacion de central.h

--- src/911.cpp
+++ src/911.cpp
@@ -6,14 +6,26 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
-#include "oficial.h"
+#include "comisaria.h"
+#include "Hospital.h"
+#include "Central.h"
 
+using namespace std;
+
+int max_patrullas = 10;
 int main() {
 
-	Oficial of = Oficial(1,1);
-	cout << "Tipo de oficial:" <<of.getTipoOficial() << endl;
-	cout << "Jornada:" << of.getJornada() << endl;
-	cout << "Arma: " << of.getArma() << endl;
+	pthread_t hilosCentral[3];
+	for (int i = 0; i < 3; i++) {
+		//Asi se realiza el llamado del hilo de hospital
+		//pthread_create(&hilosPatrulla[i],nullptr,Hospital::atenderMedico,nullptr);
+		//Asi se realiza el llamado del hilo de comisaria
+		pthread_create(&hilosCentral[i], nullptr,Central::atenderLlamada, nullptr);
+	}
+
+	pthread_join(hilosCentral[0], nullptr);
+	pthread_join(hilosCentral[1], nullptr);
+	pthread_join(hilosCentral[2], nullptr);
+
 	return 0;
 }
